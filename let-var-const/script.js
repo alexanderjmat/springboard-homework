3caea7daa0cb354750f418c522b7ecdcfe83b837
alexanderjmat <alexanderjmatula@gmail.com>
2022-06-27 11:11:58 -0400

    variables es2015 homework

---
+++ let-var-const/script.js
@@ -0,0 +1,17 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+// What is the difference between var and let?
+//var can be redeclared, reassigned, hoisted, and exists in function-scope, whereas let cannot be reclared or hoisted, and it exists in block-scope.
+
+// What is the difference between var and const?
+//var can be redeclared, reassigned, hoisted, and exists in function-scope, whereas const cannot be redeclared, reassigned, or hoisted, and it exists in block-scope
+
+// What is the difference between let and const?
+//Let can be reassigned but const cannot be reassigned
+
+// What is hoisting?
+//Hoisting is when a variable can be used before it is declared or initialized
+
